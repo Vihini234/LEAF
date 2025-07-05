5164af4a5de6b86d049c8fe5c01d7ab43b8e1f11
Vihini234 <145818980+Vihini234@users.noreply.github.com>
2025-07-05 19:43:39 +0530

    Merge remote README

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1>Hello</h1>
+      <p>nobis cupiditate vel, alias nisi tenetur cumque minus, omnis porro nesciunt enim ullam. Nulla sunt incidunt cum?</p>
+    </>
+  );
+}
+
+export default App;
